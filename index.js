b7c633e617e2799c04418da70f185b809a1385fd
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2022-06-23 18:05:07 +0530

    Setup the Express Router

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-// This is the Import section all the imports will be listed here
+// This is the Import section
 const express = require('express');
 
 // Setting up the Express Server
 const app = express();
 const port = 8000;
+
+// This is the Middleware Section
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
