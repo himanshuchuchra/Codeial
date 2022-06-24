b88d4d46f9300cb13f58b0d88cc2482fb883f5e6
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2022-06-24 12:11:38 +0530

    Install passport, passport-local and Implement Local Strategy

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+}, function(email, password, done){
+    User.findOne({email: email}, function(err, user){
+        if(err){
+            console.log(`Error in finding user for Sign-In: ${err}`);
+            return done(err);
+        }
+        if(!user || user.password != password){
+            console.log('Invalid Username/Password');
+            return done(null,false);
+        }
+        return done(null, user);
+    });
+}));
