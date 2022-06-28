cf2e66a0c5be7b9776ac0d6ef0fbfcf987c0a384
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2022-06-29 00:11:48 +0530

    Display all posts on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,14 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Codeial | Home"
+    Post.find({}, function(err, posts){
+        if(err){
+            console.log(`Error in finding Posts --> Home Controller: ${err}`);
+            return;
+        }
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
 }
