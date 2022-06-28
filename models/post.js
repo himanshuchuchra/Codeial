3da38243f3adc418a8e4805555fac800450506cd
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2022-06-28 16:30:14 +0530

    Create Controller Action and Route for post form

--- models/post.js
+++ models/post.js
@@ -13,6 +13,6 @@ const postSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const Post = mongoose.Model('Post', postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
