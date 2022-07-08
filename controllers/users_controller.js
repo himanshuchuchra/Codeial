4b2bbdb4c085aefdb6165e1be5fd41cfe6792deb
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2022-07-08 12:21:56 +0530

    Apply async await functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 
-module.exports.signUp = function(req, res){
-    if(req.isAuthenticated()){
+module.exports.signUp = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
     return res.render('user_sign_up', {
@@ -9,8 +9,8 @@ module.exports.signUp = function(req, res){
     });
 }
 
-module.exports.signIn = function(req, res){
-    if(req.isAuthenticated()){
+module.exports.signIn = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
     return res.render('user_sign_in', {
@@ -18,57 +18,60 @@ module.exports.signIn = function(req, res){
     });
 }
 
-module.exports.profile = function(req, res){
-    User.findById(req.params.id, function(err, user){
+module.exports.profile = async function (req, res) {
+    try {
+        let user = await User.findById(req.params.id);
         return res.render('user_profile', {
             title: 'Codeial | Profile Section',
             profile_user: user
         });
-    });
+    } catch (err) {
+        console.log(`Error: ${err}`);
+        return;
+    }
 }
 
-module.exports.create = function(req, res){
-    if(req.body.password != req.body.confirm_password){
-        return res.redirect('back');
-    }
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){
-            console.log(`Error in finding User during Sign-Up: ${err}`);
-            return;
-        }
-        if(!user){
-            User.create(req.body, function(err, user){
-                if(err){
-                    console.log(`Error in Signing Up the User: ${err}`);
-                    return;
-                }
-                return res.redirect('/users/sign-in');
-            });
-        }else{
+module.exports.create = async function (req, res) {
+    try {
+        if (req.body.password != req.body.confirm_password) {
             return res.redirect('back');
         }
-    });
+        let user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            await User.create(req.body);
+            return res.redirect('/users/sign-in');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log(`Error: ${err}`);
+        return;
+    }
 }
 
-module.exports.createSession = function(req, res){
+module.exports.createSession = function (req, res) {
     return res.redirect('/');
 }
 
-module.exports.destroySession = function(req, res, next){
-    req.logout(function(err){
-        if(err){
+module.exports.destroySession = function (req, res, next) {
+    req.logout(function (err) {
+        if (err) {
             return next(err);
         }
         return res.redirect('/');
     });
 }
 
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+module.exports.update = async function (req, res) {
+    try {
+        if (req.user.id == req.params.id) {
+            await User.findByIdAndUpdate(req.params.id, req.body);
             return res.redirect('back');
-        });
-    }else{
-        return res.status(401).send('Unauthorised');
+        } else {
+            return res.status(401).send('Unauthorised');
+        }
+    } catch (err) {
+        console.log(`Error: ${err}`);
+        return;
     }
 }
